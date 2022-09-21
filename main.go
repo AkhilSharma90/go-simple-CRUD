064b93ae371a45b33154dfb8379b2185a240685b
akhil <akhilsails@gmail.com>
2022-09-21 17:51:40 +0530

    working, tested code

--- main.go
+++ main.go
@@ -2,10 +2,9 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
+
 	"log"
 	"net/http"
-	"strconv"
 )
 
 type Todo struct {
@@ -20,38 +19,11 @@ func main() {
 		rw.Header().Add("Content-Type", "application/json")
 		rw.Header().Add("Access-Control-Allow-Origin", "*")
 
-		query := r.URL.Query()
-		id, _ := strconv.Atoi(query.Get("id"))
-		fmt.Println(id)
-
 		switch r.Method {
 		case "GET":
 			rw.WriteHeader(http.StatusOK)
 			json.NewEncoder(rw).Encode(todos)
-		case "POST":
-			var todo Todo
-			json.NewDecoder(r.Body).Decode(&todo)
-			todos = append(todos, todo)
-			rw.WriteHeader(http.StatusOK)
-			json.NewEncoder(rw).Encode(todo)
-		case "DELETE":
-			for index, todo := range todos {
-				if todo.ID == id {
-					todos = append(todos[:index], todos[index+1:]...)
-					rw.WriteHeader(http.StatusOK)
-					rw.Write([]byte(`{"message": "Success to delete todo"}`))
-				}
-			}
-		case "PUT":
-			for index, todo := range todos {
-				if todo.ID == id {
-					json.NewDecoder(r.Body).Decode(&todo)
-					todos[index].ID = todo.ID
-					todos[index].Task = todo.Task
-					rw.WriteHeader(http.StatusOK)
-					rw.Write([]byte(`{"message": "Success to update todo"}`))
-				}
-			}
+
 		}
 	})
 
